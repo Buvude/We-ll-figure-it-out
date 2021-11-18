83dff0f880ed07b0e3363373990aca2461f3257e
Buvude <90856761+Buvude@users.noreply.github.com>
2021-11-18 15:09:07 -0500

    basic gameplan

--- We'll figure it out/Assets/Scripts/PlayerMovement.cs
+++ We'll figure it out/Assets/Scripts/PlayerMovement.cs
@@ -1,41 +1,118 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float vert, horz;
-    public GameObject self;
-    public float Negativespeeduntouched, NegativeSpeed;
-    public bool hoeEquiped;
-    private List<bool> InventoryItems = new List<bool>();
-    private List<bool> InventoryIndex = new List<bool>();
-    // Start is called before the first frame update
+    public Canvas Stir, Skim, Ladel;
+    public int StartingPercentage = 50, boilingHeat=1, PlayerStrength=1,otherGoal=0, otherGoalGoal=10, otherGoalNumber=0, otherGoalGoalNumber=3;
+    private int currentPercentage,CountDown;
+    public Slider stirPercentage,skimPercentage;
+    private bool lost = false;
+    public Text oGTXT, sTXT;
     void Start()
     {
-        
+        stirPercentage.value = StartingPercentage;
+        currentPercentage = StartingPercentage;
+        StartCoroutine("StirGoingDown");
+        oGTXT.text = otherGoal + "/" + otherGoalGoal + " Right click when this is complete";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        vert = Input.GetAxis("Vertical");
-        horz = Input.GetAxis("Horizontal");
-        self.transform.Translate( new Vector3(horz, vert)/NegativeSpeed);
-        if (Input.GetKey(KeyCode.Alpha1)||Input.GetKey(KeyCode.Keypad1))
+        if (otherGoalNumber == otherGoalGoalNumber)
         {
-            Inventory(1);
+            oGTXT.text = otherGoal + "/" + otherGoalGoal + " Press space when complete.";
         }
 
-    }
-    public void Inventory(int index)
-    {
-        switch (index)
+        else
         {
-            case 1:
-
-                break;
+            oGTXT.text = otherGoal + "/" + otherGoalGoal + " Next Task Meter, right click when complete";
+        }
+        stirPercentage.value = currentPercentage;
+        if (Input.GetMouseButtonDown(0)&&!lost){
+            currentPercentage += PlayerStrength;
+            //otherGoal += 1;
+        }
+        if (Input.GetMouseButtonDown(1) && !lost)
+        {
+            if (otherGoal < otherGoalGoal)
+            {
+                Debug.Log("loss");
+                lost = true;
+            }
+            else
+            {
+                //Debug.Log("Skimmed");
+                StopCoroutine("StirGoDown");
+                StartCoroutine("HurryWithSkim");
+                Stir.gameObject.SetActive(false);
+                Skim.gameObject.SetActive(true);
+                otherGoal = 0;
+                
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(otherGoalNumber == otherGoalGoalNumber && otherGoal <= otherGoalGoal + 2 && otherGoal >= otherGoalGoal - 1)
+            {
+                Debug.Log("One 'point' (Ladeled)");
+                otherGoal = 0;
+                otherGoalNumber = 0;
+            }
+            else
+            {
+                Debug.Log("loss");
+                lost = true;
+            }
+        }
+        if (currentPercentage <= 10 || currentPercentage >= 90)
+        {
+            Debug.Log("loss");
+            lost = true;
+        }
+        if (otherGoal >= otherGoalGoal + 2)
+        {
+            Debug.Log("Loss");
+            lost = true;
+        }
+        if (skimPercentage.value == 100)
+        {
+            StopCoroutine("HurryWithSkim");
+            otherGoal = 0;
+            otherGoalNumber++;
+            StartCoroutine("StirGoingDown");
+            Skim.gameObject.SetActive(false);
+            Stir.gameObject.SetActive(true);
+            skimPercentage.value = 0;
+            stirPercentage.value = 50;
+        }
+    }
+    IEnumerator StirGoingDown()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            currentPercentage -= boilingHeat;
+            otherGoal++;
+        }
+        
+    }
+    IEnumerator HurryWithSkim()
+    {
+        CountDown = 3;
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            CountDown--;
+            sTXT.text = "Hurry with skimming! " + CountDown;
+            if (CountDown < 0&&skimPercentage.value<100)
+            {
+                lost = true;
+                Debug.Log("Lost");
+            }
         }
     }
 }
